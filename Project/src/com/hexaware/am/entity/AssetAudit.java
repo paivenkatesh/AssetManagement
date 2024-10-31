855ffe561f0420d5d20372b1e9ed4829ae4a0435
Venkatesh <paivenkatesh.vp@gmail.com>
2024-10-31 15:23:08 +0530

    Eclipse Project with packages and entity classes

---
+++ Project/src/com/hexaware/am/entity/AssetAudit.java
@@ -0,0 +1,91 @@
+package com.hexaware.am.entity;
+
+/*
+* Author: Venkatesh Pai 
+* Date: 31-10-2024
+* Entity classes created
+*/
+import java.util.*;
+import java.sql.Time;
+
+public class AssetAudit {
+	
+	private int auditID;
+	private int employeeID;
+	private int assetID;
+	private String auditStatus;
+	private Time requestedAt;
+	private Time updatedAt;
+	
+	public AssetAudit() {
+		super();
+	}
+
+	public AssetAudit(int auditID, int employeeID, int assetID, String auditStatus, Time requestedAt, Time updatedAt) {
+		super();
+		this.auditID = auditID;
+		this.employeeID = employeeID;
+		this.assetID = assetID;
+		this.auditStatus = auditStatus;
+		this.requestedAt = requestedAt;
+		this.updatedAt = updatedAt;
+	}
+
+	public int getAuditID() {
+		return auditID;
+	}
+
+	public void setAuditID(int auditID) {
+		this.auditID = auditID;
+	}
+
+	public int getEmployeeID() {
+		return employeeID;
+	}
+
+	public void setEmployeeID(int employeeID) {
+		this.employeeID = employeeID;
+	}
+
+	public int getAssetID() {
+		return assetID;
+	}
+
+	public void setAssetID(int assetID) {
+		this.assetID = assetID;
+	}
+
+	public String getAuditStatus() {
+		return auditStatus;
+	}
+
+	public void setAuditStatus(String auditStatus) {
+		this.auditStatus = auditStatus;
+	}
+
+	public Time getRequestedAt() {
+		return requestedAt;
+	}
+
+	public void setRequestedAt(Time requestedAt) {
+		this.requestedAt = requestedAt;
+	}
+
+	public Time getUpdatedAt() {
+		return updatedAt;
+	}
+
+	public void setUpdatedAt(Time updatedAt) {
+		this.updatedAt = updatedAt;
+	}
+
+	@Override
+	public String toString() {
+		return "AssetAudit [auditID=" + auditID + ", employeeID=" + employeeID + ", assetID=" + assetID
+				+ ", auditStatus=" + auditStatus + ", requestedAt=" + requestedAt + ", updatedAt=" + updatedAt + "]";
+	}
+	
+	
+	
+
+}
---
+++ Project/src/com/hexaware/am/entity/AssetBorrowing.java
@@ -0,0 +1,91 @@
+package com.hexaware.am.entity;
+
+/*
+ * Author: Venkatesh Pai 
+ * Date: 31-10-2024
+ * Entity classes created
+ */
+
+import java.sql.Time;
+
+public class AssetBorrowing {
+	
+	private int borrowingID;
+	private int employeeID;
+	private int assetID;
+	private Time borrowedAt;
+	private Time returnedAt;
+	private String status;
+	
+	public AssetBorrowing() {
+		super();
+	}
+
+	public AssetBorrowing(int borrowingID, int employeeID, int assetID, Time borrowedAt, Time returnedAt,
+			String status) {
+		super();
+		this.borrowingID = borrowingID;
+		this.employeeID = employeeID;
+		this.assetID = assetID;
+		this.borrowedAt = borrowedAt;
+		this.returnedAt = returnedAt;
+		this.status = status;
+	}
+
+	public int getBorrowingID() {
+		return borrowingID;
+	}
+
+	public void setBorrowingID(int borrowingID) {
+		this.borrowingID = borrowingID;
+	}
+
+	public int getEmployeeID() {
+		return employeeID;
+	}
+
+	public void setEmployeeID(int employeeID) {
+		this.employeeID = employeeID;
+	}
+
+	public int getAssetID() {
+		return assetID;
+	}
+
+	public void setAssetID(int assetID) {
+		this.assetID = assetID;
+	}
+
+	public Time getBorrowedAt() {
+		return borrowedAt;
+	}
+
+	public void setBorrowedAt(Time borrowedAt) {
+		this.borrowedAt = borrowedAt;
+	}
+
+	public Time getReturnedAt() {
+		return returnedAt;
+	}
+
+	public void setReturnedAt(Time returnedAt) {
+		this.returnedAt = returnedAt;
+	}
+
+	public String getStatus() {
+		return status;
+	}
+
+	public void setStatus(String status) {
+		this.status = status;
+	}
+
+	@Override
+	public String toString() {
+		return "AssetBorrowing [borrowingID=" + borrowingID + ", employeeID=" + employeeID + ", assetID=" + assetID
+				+ ", borrowedAt=" + borrowedAt + ", returnedAt=" + returnedAt + ", status=" + status + "]";
+	}
+	
+	
+
+}
