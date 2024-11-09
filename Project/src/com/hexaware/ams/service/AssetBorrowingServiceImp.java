77e48f6bc9592a3b0261d078cf5913e2eb3ce8ca
Venkatesh <paivenkatesh.vp@gmail.com>
2024-11-09 11:21:32 +0530

    Service package Interfaces and Implementation classes added

---
+++ Project/src/com/hexaware/ams/service/AssetBorrowingServiceImp.java
@@ -0,0 +1,63 @@
+package com.hexaware.ams.service;
+
+/*
+ * Author: Venkatesh Pai & Arghya Mandal
+ * Date: 09-11-2024
+ * Comment: Implemented AssetBorrowing 
+ * 
+ */
+import java.util.List;
+
+import com.hexaware.ams.entity.AssetBorrowing;
+
+public class AssetBorrowingServiceImp implements IAssetBorrowingService {
+
+	@Override
+	public List<AssetBorrowing> getAllAssetBorrowing() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public AssetBorrowing getAssetBorrowingById(int borrowingId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<AssetBorrowing> getAssetBorrowingsByEmployeeId(int employeeId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<AssetBorrowing> getAssetBorrowingsByAssetId(int assetId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<AssetBorrowing> getAssetBorrowingByStatus(String status) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int addAssetBorrowing(AssetBorrowing assetBorrowing) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int updateAssetStatus(int borrowingId, String status) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int deleteAssetBorrowing(int borrowingId) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
---
+++ Project/src/com/hexaware/ams/service/EmployeeServiceImp.java
@@ -0,0 +1,44 @@
+package com.hexaware.ams.service;
+
+/*
+ * Author: Venkatesh Pai
+ * Date: 09-11-2024
+ * Comment: Created Interface and Implemented it.
+ */
+import java.util.List;
+
+import com.hexaware.ams.entity.Employee;
+
+public class EmployeeServiceImp implements IEmployeeService {
+
+	@Override
+	public List<Employee> getAllEmployees() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Employee getEmployeeById(int employeeId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int addEmployee(Employee employee) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int updateEmployee(Employee employee) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int deleteEmployee(int employeeId) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
