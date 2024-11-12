3366497bbacda36b32e5e10e31125195393a8bfe
Venkatesh <paivenkatesh.vp@gmail.com>
2024-11-12 12:45:20 +0530

    Added Client File to Presentation Package

---
+++ Project/src/com/hexaware/ams/presentation/Client.java
@@ -0,0 +1,11 @@
+package com.hexaware.ams.presentation;
+
+public class Client {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Welcome to Asset Management Portal");
+		
+	}
+
+}
