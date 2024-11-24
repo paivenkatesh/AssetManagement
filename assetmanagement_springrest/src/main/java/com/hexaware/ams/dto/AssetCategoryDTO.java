05b2cf321298f141c5a58e38a50c436ba912a51c
Arghya Mandal <arghya12032002@gmail.com>
2024-11-24 12:36:02 +0530

    added dto package

---
+++ assetmanagement_springrest/src/main/java/com/hexaware/ams/dto/AssetCategoryDTO.java
@@ -0,0 +1,13 @@
+package com.hexaware.ams.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class AssetCategoryDTO {
+	private int categoryId;
+	private String categoryName;
+}
